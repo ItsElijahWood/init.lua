b6739293f89108a929d26f60627695c44d8f2099
ItsElijahWood <itselijahwood@gmail.com>
2025-02-26 22:59:59 +0000

    Updated docs and added LSP keybinds.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Install [ripgrep](https://github.com/BurntSushi/ripgrep) for telescope
 Install [nodejs](https://nodejs.org) for -g update to date version
+Install [luarocks](https://luarocks.github.io/luarocks/releases/) for nvim
 Install fzf for quick swapping projects
 Install intelephense using npm -g
+Install xclip if you don't have a clipboard default for your os 
 
--- lua/itselijahwood/keybinds.lua
+++ lua/itselijahwood/keybinds.lua
@@ -2,4 +2,9 @@ vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', '<leader>pf', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>pv', '<cmd>Ex<cr>', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {noremap = true, silent = true})  
+vim.keymap.set('n', '<leader>gr', vim.lsp.buf.rename, {noremap = true, silent = true})
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {noremap = true, silent = true})
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {noremap = true, silent = true})
+
 
