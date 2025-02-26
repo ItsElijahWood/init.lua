dd686db50656dc4dd509f758a2bfb1f0d80d7a5d
ItsElijahWood <itselijahwood@gmail.com>
2025-02-26 23:51:30 +0000

    Added transparent background so it reaches full page in terminal

--- lua/itselijahwood/plugins/colorscheme.lua
+++ lua/itselijahwood/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
   {
     "rose-pine/neovim",
     config = function()
+      require("rose-pine").setup({
+        disable_background = true,
+      })
       vim.cmd("colorscheme rose-pine")
     end,
   },
