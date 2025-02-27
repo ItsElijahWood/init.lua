6f8067e5b6ac7aca2b529e74248f6bb45b5c32f8
ItsElijahWood <itselijahwood@gmail.com>
2025-02-27 02:15:44 +0000

    Added telescope live grep

--- lua/itselijahwood/keybinds.lua
+++ lua/itselijahwood/keybinds.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>pf', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>pv', '<cmd>Ex<cr>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { noremap = true, silent = true })
@@ -8,3 +9,4 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = t
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<leader>f", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
+
