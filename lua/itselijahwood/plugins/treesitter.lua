cf2148bdcc783893b52f69179ac094b974405a19
ItsElijahWood <itselijahwood@gmail.com>
2025-03-12 23:59:19 +0000

    added go syntax highlighting

--- lua/itselijahwood/plugins/treesitter.lua
+++ lua/itselijahwood/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "javascript", "php"},
+      ensure_installed = { "go", "lua", "javascript", "php"},
       highlight = { enable = true },
     })
   end
