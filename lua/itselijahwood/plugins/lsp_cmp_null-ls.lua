5cf4703cb55290c2e06766bcacdc960f8829f5bb
ItsElijahWood <itselijahwood@gmail.com>
2025-07-08 23:36:29 +0100

    feat: Changed LSP language

--- lua/itselijahwood/plugins/lsp_cmp_null-ls.lua
+++ lua/itselijahwood/plugins/lsp_cmp_null-ls.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
+        ensure_installed = { "lua_ls", "ts_ls" },
       })
     end,
   },
@@ -19,7 +19,7 @@ return {
       local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
+      lspconfig.ts_ls.setup({})
     end,
   },
 
